ea8594e8a5b319fe306ca3c95eb1854c3b19e8f5
solutiongowid <shinil@gowid.com>
2026-06-09 11:54:16 +0900

    fix: cc-demo dashboard-2 rewrite rule 추가

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,8 @@ const nextConfig = {
       { source: '/card-api-landing', destination: '/card-api-landing/index.html' },
       { source: '/card-api-landing/', destination: '/card-api-landing/index.html' },
       { source: '/cosmetic/kakao-sync', destination: '/cosmetic/kakao-sync/index.html' },
+      { source: '/cc-demo/anua/dashboard-2', destination: '/cc-demo/anua/dashboard-2/index.html' },
+      { source: '/cc-demo/anua/dashboard-2/', destination: '/cc-demo/anua/dashboard-2/index.html' },
     ];
   },
 };
